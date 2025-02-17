5d4adf8bd9f3748d4d2aa3f4f5b097cc3140a3fc
Aleksandar Ristic <aleksandarristic503@gmail.com>
2025-02-17 14:29:34 +0100

    changed package naming error

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
-sudo apt install i3 lightdm x11-xserver-utils pulseaudio kitty fish nmtray \
+sudo apt install i3 lightdm x11-xserver-utils pulseaudio kitty fish nm-tray \
 libreoffice feh mpv brightnessctl ddcutil git xautolock playerctl maim zip unzip filelight fd-find \
 nemo firmware-misc-nonfree pulseaudio-module-bluetooth transmission-gtk
