d09466311e11d2f7de5fef6fad5affbb4753344f
Aleksandar Ristic <aleksandarristic503@gmail.com>
2025-04-13 14:08:36 +0200

    added font copy to setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 sudo apt install i3 lightdm x11-xserver-utils pulseaudio kitty fish nm-tray \
 libreoffice feh mpv brightnessctl ddcutil git xautolock polybar playerctl maim zip unzip filelight fd-find \
 nemo firmware-misc-nonfree pulseaudio-module-bluetooth transmission-gtk
+
+cp -r ./fonts ~/.local/share/fonts
+fc-cache
