00dc8537d5b0a7f27188bed20eb2905c331b0111
Aleksandar Ristic <aleksandarristic503@gmail.com>
2025-05-08 12:11:36 +0200

    upgraded to nvim 0.12

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -4,7 +4,7 @@ cd neovim
 make CMAKE_BUILD_TYPE=RelWithDebInfo
 cd build
 cpack -G DEB
-sudo dpkg -i --force-overwrite  nvim-linux64.deb
+sudo dpkg -i --force-overwrite ./nvim-linux-x86_64.deb
 nvim
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
