9bc528d8b32c9d83d5d4e1e9d75980b711e66343
Aleksandar Ristic <aleksandarristic503@gmail.com>
2025-05-21 19:10:05 +0200

    no more autolocking

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 sudo apt install i3 lightdm x11-xserver-utils pulseaudio kitty fish nm-tray \
-libreoffice feh mpv brightnessctl ddcutil git xautolock polybar playerctl maim zip unzip filelight fd-find \
+libreoffice feh mpv brightnessctl ddcutil git polybar playerctl maim zip unzip filelight fd-find \
 nemo firmware-misc-nonfree pulseaudio-module-bluetooth transmission-gtk
 
 cp -r ./fonts ~/.local/share/fonts
