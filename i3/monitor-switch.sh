5d772ed0efb706203f5afee59dc97436b0db530b
Aleksandar Ristic <aleksandarristic503@gmail.com>
2025-06-15 16:57:00 +0200

    new monitor logic

--- i3/monitor-switch.sh
+++ i3/monitor-switch.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 
-# Check if the HDMI-A-0 (secondary monitor) is connected
-if xrandr | grep -q "HDMI-A-0 connected"; then
-    # If HDMI-A-0 is connected, turn off the internal display and enable HDMI
-    xrandr --output eDP --off --output HDMI-A-0 --auto --set TearFree on
-else
-    # If HDMI-A-0 is not connected, turn on the internal display
+# Get current state
+HDMI_CONNECTED=$(xrandr | grep "HDMI-A-0 connected")
+E_DP_ON=$(xrandr --listmonitors | grep -q "eDP" && echo "yes" || echo "no")
+HDMI_ON=$(xrandr --listmonitors | grep -q "HDMI-A-0" && echo "yes" || echo "no")
+
+if [[ -z "$HDMI_CONNECTED" ]]; then
     xrandr --output eDP --auto --set TearFree on --output HDMI-A-0 --off
+    exit
+fi
+
+if [[ "$HDMI_ON" == "yes" && "$E_DP_ON" == "no" ]]; then
+    xrandr --output HDMI-A-0 --off --output eDP --auto --set TearFree on
+elif [[ "$HDMI_ON" == "no" && "$E_DP_ON" == "yes" ]]; then
+    xrandr --output eDP --auto --set TearFree on \
+           --output HDMI-A-0 --auto --primary --right-of eDP --set TearFree on
+else
+    xrandr --output eDP --off --output HDMI-A-0 --auto --set TearFree on
 fi
