f79f1e8d3bf9daf7af26b9d991032a49dc04e295
Aleksandar Ristic <aleksandarristic503@gmail.com>
2025-06-22 15:37:52 +0200

    changed some things a bit

--- setup.sh
+++ setup.sh
@@ -1,9 +1,7 @@
 sudo apt install i3 lightdm x11-xserver-utils pulseaudio kitty fish nm-tray \
 libreoffice feh mpv brightnessctl ddcutil git polybar playerctl maim zip unzip filelight fd-find \
 nemo firmware-misc-nonfree pulseaudio-module-bluetooth transmission-gtk \
-pipx pandoc zathura texlive-latex-base texlive-xetex picom
+pipx zathura picom
 
 cp -r ./fonts ~/.local/share/fonts
 fc-cache
-
-# pipx install i3-swallow
