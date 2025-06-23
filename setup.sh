696c7a86301c0a81a43fd8a196793005cff0723d
Aleksandar Ristic <aleksandarristic503@gmail.com>
2025-06-23 12:46:53 +0200

    better setup + zsh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,32 @@
-sudo apt install i3 lightdm x11-xserver-utils pulseaudio kitty fish nm-tray \
+# basic packages
+sudo apt install i3 lightdm x11-xserver-utils pulseaudio kitty zsh fzf nm-tray \
 libreoffice feh mpv brightnessctl ddcutil git polybar playerctl maim zip unzip filelight fd-find \
-nemo firmware-misc-nonfree pulseaudio-module-bluetooth transmission-gtk \
-pipx zathura picom
+pipx zathura picom ranger fzf zoxide \
+firmware-misc-nonfree pulseaudio-module-bluetooth transmission-gtk \
 
+# neovim
+sudo apt install ninja-build gettext cmake unzip curl build-essential
+git clone https://github.com/neovim/neovim
+cd neovim
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+cd build
+cpack -G DEB
+sudo dpkg -i --force-overwrite ./nvim-linux-x86_64.deb
+nvim
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+cd
+
+# fonts
 cp -r ./fonts ~/.local/share/fonts
 fc-cache
+
+# zaread
+git clone https://github.com/paoloap/zaread
+cd zaread
+sudo make install
+cd ..
+rm -rf zaread
+
+# zsh
+ln -s ~/.config/.zshrc ./.zshrc   
