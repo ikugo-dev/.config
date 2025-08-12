516954376cf8e63923198dbd8c259a1944d62555
Aleksandar Ristic <aleksandarristic503@gmail.com>
2025-08-12 14:24:21 +0200

    better dual monitor support

---
+++ i3/detect-monitor.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+HDMI_CONNECTED=$(xrandr | grep "HDMI-A-0 connected")
+if [[ -z "$HDMI_CONNECTED" ]]; then
+    echo "NONE"
+fi
+
+E_DP_ON=$(xrandr --listmonitors | grep -q "eDP" && echo "yes" || echo "no")
+HDMI_ON=$(xrandr --listmonitors | grep -q "HDMI-A-0" && echo "yes" || echo "no")
+if [[ "$E_DP_ON" == "yes" && "$HDMI_ON" == "yes"  ]]; then
+    echo "BOTH"
+elif [[ "$E_DP_ON" == "yes" && "$HDMI_ON" == "no"  ]]; then
+    echo "PRIMARY"
+elif [[ "$E_DP_ON" == "no" && "$HDMI_ON" == "yes"  ]]; then
+    echo "SECONDARY"
+elif [[ "$E_DP_ON" == "no" && "$HDMI_ON" == "no"  ]]; then
+    echo "NONE"
+fi
--- i3/monitor-switch.sh
+++ i3/monitor-switch.sh
@@ -1,22 +1,13 @@
 #!/bin/bash
+MONITOR_STATE=./detect-monitor.sh
 
-# Get current state
-HDMI_CONNECTED=$(xrandr | grep "HDMI-A-0 connected")
-E_DP_ON=$(xrandr --listmonitors | grep -q "eDP" && echo "yes" || echo "no")
-HDMI_ON=$(xrandr --listmonitors | grep -q "HDMI-A-0" && echo "yes" || echo "no")
-
-if [[ -z "$HDMI_CONNECTED" ]]; then
-    xrandr --output eDP --auto --set TearFree on --output HDMI-A-0 --off
-    exit
-fi
-
-if [[ "$HDMI_ON" == "yes" && "$E_DP_ON" == "no" ]]; then
+if [[ $MONITOR_STATE == "PRIMARY" ]]; then
     xrandr --output eDP --auto --set TearFree on \
            --output HDMI-A-0 --off
-elif [[ "$HDMI_ON" == "no" && "$E_DP_ON" == "yes" ]]; then
+elif [[ $MONITOR_STATE == "SECONDARY" ]]; then
     xrandr --output eDP --auto --set TearFree on \
            --output HDMI-A-0 --auto --primary --right-of eDP --set TearFree on
-else
+elif [[ $MONITOR_STATE == "BOTH" ]]; then
     xrandr --output eDP --off \
            --output HDMI-A-0 --auto --set TearFree on
 fi
