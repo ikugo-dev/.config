66a431a65d08a15c60a5ecc591142fde98e6fcea
Aleksandar Ristic <aleksandarristic503@gmail.com>
2025-09-27 15:58:19 +0200

    switching to sway

---
+++ setup-sway.sh
@@ -0,0 +1,65 @@
+# fuck that beep sound
+echo "blacklist pcspkr" | sudo tee /etc/modprobe.d/nobeep.conf
+
+# basic packages
+sudo apt install pulseaudio nm-tray zram-tools psmisc \
+sway jq wl-clipboard grim slurp \
+firmware-misc-nonfree pulseaudio-module-bluetooth kitty fish \
+ninja-build gettext cmake unzip curl build-essential \
+firefox-esr feh mpv git openssh-client polybar zip filelight fd-find bat tree \
+pipx libreoffice zathura picom ranger transmission-gtk \
+brightnessctl ddcutil gammastep playerctl
+
+
+sudo chsh -s "/usr/bin/fish"
+
+# neovim
+cd ~/.config || exit
+git clone https://github.com/neovim/neovim
+cd neovim || exit
+make CMAKE_BUILD_TYPE=RelWithDebInfo
+cd build || exit
+cpack -G DEB
+sudo dpkg -i --force-overwrite ./nvim-linux-x86_64.deb
+nvim
+sleep 1
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+cd ~/.config || exit
+sudo rm -rf neovim
+
+# fonts
+sudo cp -r ./fonts ~/.local/share/fonts
+fc-cache
+
+# colemak layout
+sudo cp ~/.config/colemak-dh-arts /usr/share/X11/xkb/symbols/
+
+# zaread
+git clone https://github.com/paoloap/zaread
+cd zaread || exit
+sudo make install
+cd ..
+rm -rf zaread
+
+# i3 util
+pipx install i3-swallow
+
+# image preview on ranger
+sudo apt install python3-pillow
+
+# cursor
+git clone https://github.com/uloco/numix-cursor.git
+sudo mv numix-cursor/theme/Numix-Cursor /usr/share/icons
+echo -e "[Icon Theme]\nName=Numix-Cursor\nInherits=Numix-Cursor" \
+    > /usr/share/icons/default/index.theme   
+rm -rf numix-cursor
+export XCURSOR_THEME=Numix-Cursor
+export XCURSOR_SIZE=16   
+
+# fish
+fish
+curl -sL https://raw.githubusercontent.com/jorgebucaran/fisher/main/functions/fisher.fish | source && fisher install jorgebucaran/fisher
+fisher install IlanCosman/tide@v6
+fisher install jhillyerd/plugin-git
+exit
